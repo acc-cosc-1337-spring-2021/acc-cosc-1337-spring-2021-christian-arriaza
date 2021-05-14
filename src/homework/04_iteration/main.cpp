cde2babf373784ccc9d48eb5c9152d1543bb9c95
christian-arriaza <carriaza6302@gmail.com>
2021-05-13 22:33:12 -0500

    Homework 5

--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
+#include "dna.h"
+#include "dna.cpp"
 
+#include <iostream>
 //write using statements
-
+using namespace std;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +15,39 @@ user enters a y or Y.
 */
 int main() 
 {
+	int choice = 0;
+	char repeat;
+
+	string dna_string;
+
+	do
+	{
+		cout << "GET GC CONTENT (1)" << '\n';
+		cout << "GET DNA COMPLEMENT (2)" << '\n';
+		cout << "PLEASE CHOOSE (1) OR (2): " << '\n';
+		cin >> choice;
+
+		while (!(choice == 1 || choice == 2))
+		{
+			cout << "INVALID CHOICE, TRY AGAIN: ";
+			cin >> choice;
+		}
+
+		cout << "ENTER DNA STRING: ";
+		cin >> dna_string;
+
+		if (choice ==1)
+		{
+			cout << "GC CONTENT OF THE STRING IS: " << get_gc_content(dna_string) * 100 << "%" << '\n';
+		}
+		else
+		{
+			cout << "THE COMPLEMENT OF THE DNA STRING IS: " << get_dna_complement(dna_string) << '\n';
+		}
+
+		cout << "WOULD YOU LIKE TO PROCESS ANOTHER STRING? ENTER Y TO CONTINUE: ";
+		cin >> repeat;
+	} while (repeat == 'Y' || repeat == 'y');
+
 	return 0;
 }
--- test/homework_test/04_iteration_test/04_iteration_tests.cpp
+++ test/homework_test/04_iteration_test/04_iteration_tests.cpp
@@ -1,7 +1,42 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "dna.h"
+
+using namespace std;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify get_gc_content function")
+{
+	string dna;
+
+	dna = "AGCTATAG";
+	REQUIRE(get_gc_content(dna) == 0.375);
+
+	dna = "CGCTATAG";
+	REQUIRE(get_gc_content(dna) == 0.50);
+}
+
+TEST_CASE("Verify get_reverse_string function")
+{
+	string dna;
+
+	dna = "AGCTATAG";
+	REQUIRE(get_reverse_string(dna) == "GATATCGA");
+	
+	dna = "CGCTATAG";
+	REQUIRE(get_reverse_string(dna) == "GATATCGC");
+}
+
+TEST_CASE("Verify get_dna_complement function")
+{
+	string dna;
+
+	dna = "AAAACCCGGT";
+	REQUIRE(get_dna_complement(dna) == "ACCGGGTTTT");
+
+	dna = "CCCGGAAAAT";
+	REQUIRE(get_dna_complement(dna) == "ATTTTCCGGG");
+}
